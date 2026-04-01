e2bde643a6dc0ce9f5c15afa1a779c0c5e007834
princesafeona <foubrown@gmail.com>
2026-04-01 17:46:01 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const messageButton = document.getElementById("messageButton");
-const messageText = document.getElementById("messageText");
-
-messageButton.addEventListener("click", () => {
-  messageText.textContent = "Nice work! Your JavaScript is connected.";
-});
